998ce4f0cf9e7d4526f154894cf3f801e083907d
edbkva <eddbkva00@gmail.com>
2021-08-20 21:18:33 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final String S1 = "My name is ";
+        String name = "Ademi";
+        System.out.println(S1 + name);
+    }
+}
